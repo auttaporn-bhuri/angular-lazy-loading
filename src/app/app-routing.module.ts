b30a6e4e358963941b40cc133db846de24a2c6a4
auttaporn-bhuri <auttaporn@gmail.com>
2023-05-26 10:33:04 +0700

    learn angular lazy loading and create feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'customers',
+    loadChildren: () => import('./customers/customers.module').then(x => x.CustomersModule)
+  },
+  { path:'orders',
+    loadChildren: () => import('./orders/orders.module').then(x => x.OrdersModule)
+  },
+  { path:'',
+    redirectTo: 'customers',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
